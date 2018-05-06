800651337c30fa7196aaeba0909c1bcf9b1fc8fb
PS <szczepanski.piotr@outlook.com>
2018-05-06 21:22:15 +0200

    Struktura projektu w Android Studio

---
+++ app/src/main/java/sdp/project/twitter/Login.java
@@ -0,0 +1,15 @@
+package sdp.project.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sdp.project.tweeter.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/sdp/project/twitter/TweetItem.java
@@ -0,0 +1,23 @@
+package sdp.project.twitter;
+
+public class TweetItem {
+    public String tweet_id;
+    public String tweet_text;
+    public String tweet_picture;
+    public String tweet_date;
+    public String user_id;
+    public String username;
+    public String picture_path;
+
+    TweetItem(String tweet_id, String tweet_text, String tweet_picture,
+              String tweet_date, String user_id, String username , String picture_path)
+    {
+        this.tweet_id = tweet_id;
+        this.tweet_text = tweet_text;
+        this.tweet_picture = tweet_picture;
+        this.user_id = user_id;
+        this.username = username;
+        this.picture_path = picture_path;
+        this.tweet_date = tweet_date;
+    }
+}
