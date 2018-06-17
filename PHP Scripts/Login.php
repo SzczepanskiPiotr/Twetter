16493a2df80ec50d19aed0de6ce2b50eccfb40ef
PS <szczepanski.piotr@outlook.com>
2018-06-17 23:48:31 +0200

    Hashowanie haseł - login fix

--- PHP Scripts/Login.php
+++ PHP Scripts/Login.php
@@ -11,14 +11,6 @@ if(! $result)
  //get data from database
 $output=array();
 
-$query1 = "select password from users where username= '". $_GET['username'] ."'";
-$result1 = mysqli_query($connect, $query1);
-
-$row1  = mysqli_fetch_row($result1);
-$hashedPassword1 = $row1[0];
-
-
-if (password_verify($_GET['password'],$hashedPassword1)) {
 	while($row=  mysqli_fetch_assoc($result))
 	{
 		$output[]=$row;  //$row['id']
@@ -26,14 +18,20 @@ if (password_verify($_GET['password'],$hashedPassword1)) {
 	}
 	if ($output) 
 	{
-		print( "{'msg':'Pass Login'". ",'info':'". json_encode($output) ."'}");// this will print the output in json
+		$query1 = "select password from users where username= '". $_GET['username'] ."'";
+		$result1 = mysqli_query($connect, $query1);
+		$row1  = mysqli_fetch_row($result1);
+		$hashedPassword1 = $row1[0];
+		
+		if (password_verify($_GET['password'],$hashedPassword1)) {
+			print( "{'msg':'Pass Login'". ",'info':'". json_encode($output) ."'}");// this will print the output in json
+		}else {
+		print("{'msg':'invalid password'}");
+		}
 	}
 	else{
 		print("{'msg':' cannot login'}");
 	}
-} else {
-		print("{'msg':'invalid password'}");
-}
 
 // 4 clear
 mysqli_free_result($result);
